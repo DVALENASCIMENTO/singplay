82a93da685e6fa21673cf8c3b9f1e5aed942f14c
Diego <diegonascimentocredito@gmail.com>
2025-06-18 21:35:49 -0400

    add audio musics

--- audios/audios.js
+++ audios/audios.js
@@ -1,22 +1,66 @@
 const audioDatabase = [
     {
         id: 1,
-        src: "audios/Wish You Were Here Backing Track (No Vocals).mp3"
+        src: "audios/coisaLinda.mp3"
     },
     {
         id: 2,
-        src: "audios/aguas_de_marco.mp3"
+        src: "audios/soOsLoucosSabem.mp3"
     },
     {
         id: 3,
-        src: "audios/chega_de_saudade.mp3"
+        src: "audios/oSol.mp3"
     },
     {
         id: 4,
-        src: "audios/construcao.mp3"
+        src: "audios/ameiTeVer.mp3"
     },
     {
         id: 5,
-        src: "audios/aquarela.mp3"
+        src: "audios/rude.mp3"
+    },
+    {
+        id: 6,
+        src: "audios/isThisLove.mp3"
+    },
+    {
+        id: 7,
+        src: "audios/vamosFugir.mp3"
+    },
+    {
+        id: 8,
+        src: "audios/dontDreamItsOver.mp3"
+    },
+    {
+        id: 9,
+        src: "audios/standByMe-everyBreathYouTake.mp3"
+    },
+    {
+        id: 10,
+        src: "audios/WithOrWithoutYou.mp3"
+    },
+    {
+        id: 11,
+        src: "audios/youAreTheOne.mp3"
+    },
+    {
+        id: 12,
+        src: "audios/takeOnMe.mp3"
+    },
+    {
+        id: 13,
+        src: "audios/aLittleRespect.mp3"
+    },
+    {
+        id: 14,
+        src: "audios/africa.mp3"
+    },
+    {
+        id: 15,
+        src: "audios/getLucky.mp3"
+    },
+    {
+        id: 16,
+        src: "audios/celebration.mp3"
     }
 ];
--- songs/songs.js
+++ songs/songs.js
@@ -1,44 +1,98 @@
-const songsDatabase = [
+const songsDatabase = [ 
     {
         id: 1,
-        title: "Wish You Were Here",
-        artist: "Pink Floyd",
-        lyrics: `So, so you think you can tell
-Heaven from hell?
-Blue skies from pain?
-Can you tell a green field
-From a cold steel rail?
-A smile from a veil?
-Do you think you can tell?
-
-Did they get you to trade
-Your heroes for ghosts?
-Hot ashes for trees?
-Hot air for a cool breeze?
-Cold comfort for change?
-Did you exchange
-A walk on part in the war
-For a lead role in a cage?
-
-How I wish
-How I wish you were here
-We're just two lost souls
-Swimming in a fish bowl year after year
-Running over the same old ground
-What have we found?
-The same old fears
-Wish you were here`
+        title: "Coisa Linda",
+        artist: "Tiago Iorc",
+        lyrics: ""
     },
     {
         id: 2,
-        title: "Águas de Março",
-        artist: "Tom Jobim",
-        lyrics: `É pau, é pedra, é o fim do caminho
-É um resto de toco, é um pouco sozinho
-É um caco de vidro, é a vida, é o sol
-É a noite, é a morte, é o laço, é o anzol
-...` 
+        title: "Só os Loucos Sabem",
+        artist: "Charlie Brown Jr.",
+        lyrics: ""
     },
-    
-    // Continue com as demais músicas...
+    {
+        id: 3,
+        title: "O Sol",
+        artist: "Vitor Kley",
+        lyrics: ""
+    },
+    {
+        id: 4,
+        title: "Amei Te Ver",
+        artist: "Tiago Iorc",
+        lyrics: ""
+    },
+    {
+        id: 5,
+        title: "Rude",
+        artist: "MAGIC!",
+        lyrics: ""
+    },
+    {
+        id: 6,
+        title: "Is This Love",
+        artist: "Bob Marley",
+        lyrics: ""
+    },
+    {
+        id: 7,
+        title: "Vamos Fugir",
+        artist: "Gilberto Gil",
+        lyrics: ""
+    },
+    {
+        id: 8,
+        title: "Don’t Dream It’s Over",
+        artist: "Crowded House",
+        lyrics: ""
+    },
+    {
+        id: 9,
+        title: "Stand By Me / Every Breath You Take",
+        artist: "Ben E. King / The Police",
+        lyrics: ""
+    },
+    {
+        id: 10,
+        title: "With or Without You",
+        artist: "U2",
+        lyrics: ""
+    },
+    {
+        id: 11,
+        title: "You Are the One",
+        artist: "A-ha",
+        lyrics: ""
+    },
+    {
+        id: 12,
+        title: "Take on Me",
+        artist: "A-ha",
+        lyrics: ""
+    },
+    {
+        id: 13,
+        title: "A Little Respect",
+        artist: "Erasure",
+        lyrics: ""
+    },
+    {
+        id: 14,
+        title: "Africa",
+        artist: "Toto",
+        lyrics: ""
+    },
+    {
+        id: 15,
+        title: "Get Lucky",
+        artist: "Daft Punk feat. Pharrell Williams",
+        lyrics: ""
+    },
+    {
+        id: 16,
+        title: "Celebration",
+        artist: "Kool & The Gang",
+        lyrics: ""
+    }
 ];
